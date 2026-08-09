f8f1533e7ccb9b5650d61597c4cc7a24b8394e5b
Fazliddin Hamzayev <hamzayev2008@gmail.com>
2026-08-09 21:33:52 +0500

    augmentation has been added to transforms.py

--- src/transforms.py
+++ src/transforms.py
@@ -5,9 +5,10 @@ def get_transform(augmentation=False):
 
     transform_list = []
 
-    # Здесь потом можно добавить аугментации
     if augmentation:
-        pass
+        transform_list.append(transforms.RandomHorizontalFlip(p=0.5))
+        transform_list.append(transforms.RandomRotation(degrees=15))
+        transform_list.append(transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1))
 
     transform_list.extend([
         transforms.ToTensor(),
