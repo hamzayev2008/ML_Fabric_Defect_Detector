242fe06c8047f504e7815c20ae0d85b4b1b61e88
Fazliddin Hamzayev <hamzayev2008@gmail.com>
2026-08-10 16:33:42 +0500

    updated last time before educaiting model

--- src/image_utils.py
+++ src/image_utils.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from PIL import Image
 
 def load_image_from_bytes(image_bytes, image_size, transform):
     numpy_array = np.frombuffer(image_bytes, np.uint8)
@@ -13,5 +14,6 @@ def load_image(path, image_size, transform):
 def process_image(image, image_size, transform):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image = cv2.resize(image, (image_size, image_size))
+    image = Image.fromarray(image)
     image = transform(image)
     return image
