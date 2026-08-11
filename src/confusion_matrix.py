39f98932786a1b5529380fb8c7cb3591063c8f20
Fazliddin Hamzayev <hamzayev2008@gmail.com>
2026-08-11 16:42:04 +0500

    prediction/recall/f1-score table has been added to confusion_matrix.py and wrong images have been made visible with actual and predicted labels

--- src/confusion_matrix.py
+++ src/confusion_matrix.py
@@ -1,7 +1,7 @@
 import torch
 import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix
-from config import MODEL_PATH, CLASSES
+from sklearn.metrics import confusion_matrix, classification_report
+from config import MODEL_PATH, CLASSES, BATCH_SIZE
 from model import TeddyClassifier
 from dataLoader import test_loader
 
@@ -9,10 +9,13 @@ model = TeddyClassifier()
 
 true_labels = []
 predicted_labels = []
+wrong_images = []
 
 state_dict = torch.load(MODEL_PATH)
 model.load_state_dict(state_dict)
 model.eval()
+
+batch_start = 0
 
 with torch.no_grad():
     for images, labels in test_loader:
@@ -20,6 +23,19 @@ with torch.no_grad():
         predicted = outputs.argmax(dim=1)
         true_labels.extend(labels.tolist())
         predicted_labels.extend(predicted.tolist())
+        wrong = predicted != labels
+        wrong_indices = wrong.nonzero().flatten().tolist()
+        for wrong_index in wrong_indices:
+            global_index = batch_start + wrong_index
+            path, actual_label = test_loader.dataset.images[global_index]
+            predicted_label = predicted[wrong_index]
+            wrong_images.append((path, actual_label, predicted_label))
+        batch_start += BATCH_SIZE
+        
+print(wrong_images)
+        
+report = classification_report(true_labels, predicted_labels)
+print(report)
 
 cm = confusion_matrix(true_labels, predicted_labels)
 
@@ -35,4 +51,17 @@ for i in range(len(CLASSES)):
 plt.xlabel('Predicted Label')
 plt.ylabel('Actual Label')
 plt.title('Confusion Matrix')
+plt.show()
+
+plt.subplot(1, 2, 1)
+plt.title(wrong_images[0][1].item(CLASSES[labels.item()]))
+plt.title(wrong_images[0][2].item(CLASSES[predicted.item()]))
+image = plt.imread(wrong_images[0][0])
+plt.imshow(image)
+plt.subplot(1, 2, 2)
+plt.title(wrong_images[1][1].item(CLASSES[labels.item()]))
+plt.title(wrong_images[1][2].item(CLASSES[predicted.item()]))
+plt.title(wrong_images[1])
+image = plt.imread(wrong_images[1][0])
+plt.imshow(image)
 plt.show()
